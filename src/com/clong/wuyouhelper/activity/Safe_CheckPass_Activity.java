69b6d2105d3447306c334f7f0870f406bf61e233
Cheng-Long <1011056799@qq.com>
2016-02-29 23:58:32 +0800

    发送短信的添加

--- src/com/clong/wuyouhelper/activity/Safe_CheckPass_Activity.java
+++ src/com/clong/wuyouhelper/activity/Safe_CheckPass_Activity.java
@@ -1,7 +1,7 @@
 package com.clong.wuyouhelper.activity;
 
-import com.clong.wuyouhelper.R;
-import com.clong.wuyouhelper.constants.Constants;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,7 +14,10 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.clong.sendmail.MainSendMail;
+import com.clong.wuyouhelper.R;
+import com.clong.wuyouhelper.constants.Constants;
 
 /**
  * 手机防盗_验证密码
@@ -118,7 +121,29 @@ public class Safe_CheckPass_Activity extends Activity {
 	 * @date: 2016-2-29-下午1:11:04
 	 */
 	public void reset(View v) {
-		Toast.makeText(this, "重置密码", Toast.LENGTH_SHORT).show();
+		// 发送重置密码123到安全邮箱
+		final Map<String, String> mailValues = new HashMap<String, String>();
+		mailValues.put("serverHost", "smtp.163.com");
+		mailValues.put("serverPort", "25");
+		mailValues.put("sendMail", "bjchenglong991@163.com");
+		mailValues.put("sendPwd", "cl091992");
+		mailValues.put("getMail", "1011056799@qq.com");
+		mailValues.put("mailTitle", "无忧助手-手机防盗重置密码");
+		mailValues.put("mailCont", "重置了手机防盗密码,新密码为：<b>123</b>");
+		mailValues.put("mailType", "html");
+
+		new Thread() {
+			public void run() {
+				try {
+					boolean result = MainSendMail.sendMail(mailValues);
+					System.out.println(result ? "发送成功" : "发送失败");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			};
+		}.start();
+		// Toast.makeText(this, result ? "发送成功" : "发送失败", Toast.LENGTH_SHORT)
+		// .show();
 	}
 
 	/**
